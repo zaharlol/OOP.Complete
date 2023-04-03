499c3db5818fa072720d6d6d7fc62df4990809d1
zaharlol <kovtun.zahar12@mail.ru>
2023-04-03 13:23:25 +0300

    Индексация. Перегрузка

---
+++ Абстракция/Program.cs
@@ -0,0 +1,23 @@
+﻿abstract class ComputerPart
+{
+    public abstract void Work();
+}
+
+class Processor : ComputerPart
+{
+    public override void Work()
+    {
+    }
+}
+class MotherBoard : ComputerPart
+{
+    public override void Work()
+    {
+    }
+}
+class GraphicCard : ComputerPart
+{
+    public override void Work()
+    {
+    }
+}
