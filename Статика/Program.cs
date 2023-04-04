89eb7efb126aff9d498247d0a3fe3f78ab9d4ea4
zaharlol <kovtun.zahar12@mail.ru>
2023-04-04 16:51:30 +0300

    Статика

---
+++ Статика/Program.cs
@@ -0,0 +1,39 @@
+﻿class Obj
+{
+    public string Name;
+    public string Description;
+
+    public static string Parent;
+    public static int DaysInWeek;
+    public static int MaxValue;
+
+    static Obj()
+    {
+        Parent = "System.Object";
+        DaysInWeek = 7;
+        MaxValue = 2000;
+    }
+}
+class Helper
+{
+    public static void Swap(ref int a, ref int b)
+    {
+        int c = a;
+        a = b;
+        b = c;
+
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 3;
+        int num2 = 58;
+
+        Helper.Swap(ref num1, ref num2);
+
+        Console.WriteLine(num1); 
+        Console.WriteLine(num2); 
+    }
+}
