3a1a913c4c6d55204579e1366ea54a041202d079
zaharlol <kovtun.zahar12@mail.ru>
2023-04-07 10:07:41 +0300

    Обобщения

---
+++ Обобщения/Program.cs
@@ -0,0 +1,35 @@
+﻿abstract class Car<TEngine> where TEngine : Engine
+{
+    public TEngine Engine;
+    public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
+    {
+
+    }
+}
+abstract class Engine { }
+class ElectricEngine : Engine { }
+class GasEngine : Engine { }
+abstract class CarPart { }
+class Battery : CarPart { }
+class Differential : CarPart { }
+class Wheel : CarPart { }
+class Record<T1, T2>
+{
+    public T1 Id;
+    public T2 Value;
+    public DateTime Date;
+}
+class ElectricCar : Car<ElectricEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
+}
+class GasCar : Car<GasEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
+}
